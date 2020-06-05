fb5d87788f2379d58b8a4b14623bbde62230b13c
Magno Juliano Gonçalves Santos <santos.magno@mrv.com.br>
2020-06-05 14:56:23 -0300

    Implementa configuração da queue do storage account

---
+++ BlobStorageContainer.Interface/IAzureQueue.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace BlobStorageContainer.Interface
+{
+    public interface IAzureQueue
+    {
+        Task Criar(string nome);
+
+        Task Deletar(string nome);
+
+        Task<bool> Existe(string nome);
+
+        Task InserirMensagem(string nome, string mensagem);
+
+        Task<string> RemoverMensagem(string nome);
+    }
+}
---
+++ BlobStorageContainer.Service/AzureQueue.cs
@@ -0,0 +1,57 @@
+﻿using BlobStorageContainer.Interface;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlobStorageContainer.Service
+{
+    public class AzureQueue : IAzureQueue
+    {
+        private readonly CloudQueueClient _queue;
+
+        public AzureQueue(CloudStorageAccount storageAccount)
+        {
+            _queue = storageAccount.CreateCloudQueueClient();
+        }
+
+        public Task Criar(string nome)
+        {
+            return _queue
+                .GetQueueReference(nome)
+                .CreateIfNotExistsAsync();
+        }
+
+        public Task Deletar(string nome)
+        {
+            return _queue
+                .GetQueueReference(nome)
+                .DeleteIfExistsAsync();
+        }
+
+        public Task<bool> Existe(string nome)
+        {
+            return _queue
+                .GetQueueReference(nome)
+                .ExistsAsync();
+        }
+
+        public Task InserirMensagem(string nome, string mensagem)
+        {
+            return _queue
+                .GetQueueReference(nome)
+                .AddMessageAsync(
+                message: new CloudQueueMessage(mensagem)
+              );
+        }
+
+        public async Task<string> RemoverMensagem(string nome)
+        {
+            var mensagens = await _queue
+                .GetQueueReference(nome)
+                .GetMessagesAsync(1);
+
+            return mensagens.FirstOrDefault()?.AsString;
+        }
+    }
+}
